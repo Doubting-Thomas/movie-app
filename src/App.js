8a2dec313b6d4213bfbd85d1bbcc341142a181e2
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-02-28 11:22:58 -0800

    feat: add functionality for generating results from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import "./App.css";
 import { useState } from "react";
+import MovieData from "./components/MovieData";
 
 function App() {
+  const [movie, getMovie] = useState([]);
+
+  async function displayMovie() {
+    const res = await fetch("https://swapi.dev/api/films/");
+    const data = await res.json();
+    console.log(data);
+
+    const movieValues = data.results.map((movieValue) => {
+      return {
+        id: movieValue.episode_id,
+        title: movieValue.title,
+        director: movieValue.director,
+        producer: movieValue.producer,
+        description: movieValue.opening_crawl,
+      };
+    });
+  }
+
   return (
     <div>
       <h1>Movie App</h1>
-      <p>This displays specific movies dynamically from an API.</p>;
+      <p>This displays specific movies dynamically from an API.</p>
+      <section>
+        <button onClick={displayMovie}>Generate Movie</button>
+      </section>
+      <section>
+        <MovieData movies={movie} />
+      </section>
     </div>
   );
 }
