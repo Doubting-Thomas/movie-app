253479e95710fd11b699d428084bd3835601020b
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-02-28 11:24:06 -0800

    chore: remove console.log test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   async function displayMovie() {
     const res = await fetch("https://swapi.dev/api/films/");
     const data = await res.json();
-    console.log(data);
 
     const movieValues = data.results.map((movieValue) => {
       return {
