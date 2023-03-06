6505ea3d16af6f4eeb3f800fe4920a86e48078ce
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-03-06 11:41:06 -0800

    fix: adjust jsx

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   async function displayMovie() {
     const res = await fetch("https://swapi.dev/api/films/");
     const data = await res.json();
+    console.log(data);
 
     const movieValues = data.results.map((movieValue) => {
       return {
@@ -18,13 +19,22 @@ function App() {
         description: movieValue.opening_crawl,
       };
     });
-    getMovie(movieValues);
+    // console.log(getMovie);
+    // console.log(movieValues);
+    // getMovie(movieValues);
+    //console.log(getMovie(movieValues[0]));
   }
+  // function check() {
+  //   console.log(7);
+  // }
+  //console.log(check(6));
 
   return (
     <div>
-      <h1>Movie App</h1>
-      <p>Click the button below to display movies.</p>
+      <div className="movie__heading">
+        <h1>Movie App</h1>
+        <p>Click the button below to display movies.</p>
+      </div>
       <section>
         <button onClick={displayMovie}>Generate Movie</button>
       </section>
