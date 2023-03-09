dca9c537f8a5d47ee3f8a3190ecc5c7c43e44701
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-03-09 11:45:14 -0800

    feat: functionality for rendering lists

---
+++ src/components/Movie.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import classes from "./Movie.module.css";
+
+const Movie = (props) => {
+  return (
+    <li className={classes.movie}>
+      <h2>{props.title}</h2>
+      <h3>{props.director}</h3>
+      <h3>{props.producer}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+};
+
+export default Movie;
