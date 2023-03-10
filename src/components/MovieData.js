bb7eb565f6624e51657e1cfa7b0d2b75af0b772f
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-03-10 11:24:48 -0800

    feat: list for rendering content

--- src/components/MovieData.js
+++ src/components/MovieData.js
@@ -1,7 +1,20 @@
 import React from "react";
+import MovieList from "./Movie";
 import classes from "./MovieData.module.css";
 
 const MovieData = () => {
-  return <li className={classes.movie}></li>;
+  return (
+    <ul className={classes.movie}>
+      {props.movies.map((movie) => (
+        <MovieList
+          key={movie.id}
+          title={movie.title}
+          director={movie.director}
+          producer={movie.producer}
+          description={movie.description}
+        />
+      ))}
+    </ul>
+  );
 };
 export default MovieData;
